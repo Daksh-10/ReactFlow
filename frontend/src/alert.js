ab83278830c5e29878b948265ac9eba48d220b7a
Daksh <dakshkaushik20040126@gmail.com>
2024-10-29 17:13:12 +0530

    updated live server

--- frontend/src/alert.js
+++ frontend/src/alert.js
@@ -1,10 +1,15 @@
-export const Alert = ({nodes, edges, isDag}) => {
-
-    return <>
+export const Alert = ({ nodes, edges, isDag }) => {
+  return (
+    <>
+      {!nodes && !edges && !isDag ? (
+        <div>Wait for 50 seconds as it is deployed on free service</div>
+      ) : (
         <div>
-            <div>Total Number of Nodes: {nodes}</div>
-            <div>Total Number of Edges: {edges}</div>
-            <div>Is it a DAG: {`${isDag}`}</div>
+          <div>Total Number of Nodes: {nodes}</div>
+          <div>Total Number of Edges: {edges}</div>
+          <div>Is it a DAG: {`${isDag}`}</div>
         </div>
+      )}
     </>
-}
+  );
+};
--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -1,36 +1,51 @@
 // submit.js
 import "./styles/submit.css";
 import { useEffect } from "react";
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import {Alert} from "./alert.js";
-import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { Alert } from "./alert.js";
+import "react-toastify/dist/ReactToastify.css";
 
-export const SubmitButton = ({Vertices, Connections}) => {
+export const SubmitButton = ({ Vertices, Connections }) => {
+  useEffect(() => {
+    console.log(Vertices, Connections);
+  }, [Vertices, Connections]);
 
-    useEffect(()=>{
-        console.log(Vertices,Connections);
-    },[Vertices, Connections])
-
-    const handleSubmit = async () => {
-        try {
-            const response = await axios.post('http://localhost:8000/pipelines/parse', {
-              nodes: Vertices,
-              edges: Connections,
-            });
-            toast.success(<Alert nodes={response.data.num_nodes} edges={response.data.num_edges} isDag={response.data.is_dag}/>);
-            
-          } 
-        catch(error){
-            console.error('Error submitting graph:', error);
-            alert('There was an error processing your request.');
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        "https://reactflow-2.onrender.com/pipelines/parse",
+        {
+          nodes: Vertices,
+          edges: Connections,
         }
+      );
+      toast.success(
+        <Alert
+          nodes={response.data.num_nodes}
+          edges={response.data.num_edges}
+          isDag={response.data.is_dag}
+        />
+      );
+    } catch (error) {
+      console.error("Error submitting graph:", error);
+      toast.success(<Alert />);
     }
+  };
 
-    return (
-        <div onClick={handleSubmit} style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <button className="submit" type="submit">Submit</button>
-            <ToastContainer position="top-center"  />
-        </div>
-    );
-}
+  return (
+    <div
+      onClick={handleSubmit}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button className="submit" type="submit">
+        Submit
+      </button>
+      <ToastContainer position="top-center" />
+    </div>
+  );
+};
